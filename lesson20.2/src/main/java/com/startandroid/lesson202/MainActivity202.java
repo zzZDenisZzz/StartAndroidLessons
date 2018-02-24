d700d73a75dd8b5f25fdd88edeab8b7ef6401060
zzZDenisZzz <CoRey2007@bigmir.net>
2018-02-24 18:05:40 +0200

    Lesson 20. Animation

---
+++ lesson20.2/src/main/java/com/startandroid/lesson202/MainActivity202.java
@@ -0,0 +1,80 @@
+package com.startandroid.lesson202;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+
+public class MainActivity202 extends AppCompatActivity {
+
+    //константы для id пунктов меню.
+    final int MENU_ALPHA_ID = 1;
+    final int MENU_SCALE_ID = 2;
+    final int MENU_TRANSLATE_ID = 3;
+    final int MENU_ROTATE_ID = 4;
+    final int MENU_COMBO_ID = 5;
+
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main202);
+
+        //находим TextView tv.
+        tv = (TextView) findViewById(R.id.tv);
+
+        //регистрируем контекстное меню для TextView tv.
+        registerForContextMenu(tv);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        switch (v.getId()) {
+            case R.id.tv:
+                //добавляем пункты меню
+                menu.add(0, MENU_ALPHA_ID, 0, "alpha");
+                menu.add(0, MENU_SCALE_ID, 0, "scale");
+                menu.add(0, MENU_TRANSLATE_ID, 0, "translate");
+                menu.add(0, MENU_ROTATE_ID, 0, "rotate");
+                menu.add(0, MENU_COMBO_ID, 0, "combo");
+                break;
+        }
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Animation anim = null;
+        //определяем какой пункт был нажат
+        //и создаём объект анимации соответствующего файла.
+        switch (item.getItemId()) {
+            case MENU_ALPHA_ID:
+                //создаём объект анимации соответствующего файла.
+                anim = AnimationUtils.loadAnimation(this, R.anim.myalpha);
+                break;
+            case MENU_SCALE_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.myscale);
+                break;
+            case MENU_TRANSLATE_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.mytrans);
+                break;
+            case MENU_ROTATE_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.myrotate);
+                break;
+            case MENU_COMBO_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.mycombo);
+                break;
+        }
+        //запускаем анимацию для компонента TextView tv.
+        tv.startAnimation(anim);
+        return super.onContextItemSelected(item);
+        //анимация читается методом .loadAnimation().
+    }
+}
